2cf0b815bc4c878eb340fa2fb8414d8b76c55522
Z-Farook <46790696+Z-Farook@users.noreply.github.com>
2021-02-25 17:09:37 +0100

    git init Dev6A Base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dev6A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
